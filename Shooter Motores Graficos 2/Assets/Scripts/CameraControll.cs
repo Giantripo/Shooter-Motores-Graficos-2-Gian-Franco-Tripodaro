a6f4e9a83c02ad836f25dee1544781ac5e8186f0
Gian Franco <giancapo2011@gmail.com>
2021-04-27 01:08:04 -0300

    Camara sigue al jugador

---
+++ Shooter Motores Graficos 2/Assets/Scripts/CameraControll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public GameObject Jugador;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Jugador.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Jugador.transform.position + offset;
+    }
+
+}
