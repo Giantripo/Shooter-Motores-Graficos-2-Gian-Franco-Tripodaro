052cb2165d95ec90e7a411e59bb72c058bfa30f3
Gian Franco <giancapo2011@gmail.com>
2021-04-28 13:55:34 -0300

    shader bomba y star mejorada

---
+++ Shooter Motores Graficos 2/Assets/Scripts/Shooting/star2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class star2 : MonoBehaviour
+{
+  public int  Cont =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Cont++;
+        }
+
+            if (collision.gameObject.CompareTag("Wall"))
+            {
+
+            if (Cont ==3)
+            {
+                Cont = 0;
+                Destroy(this.gameObject);
+            }
+           
+            
+            }
+    }
+}
